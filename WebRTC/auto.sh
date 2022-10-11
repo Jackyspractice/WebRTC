e9c066ab77364f0ad1ab3e43fe00b0c184fae67c
Jacky's Zenbook <s10828152@cycu.edu.tw>
2022-10-11 22:03:10 +0800

    auto

--- WebRTC/auto.sh
+++ WebRTC/auto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gnome-terminal --"/home/jacky/Desktop/WebRTC/WebRTC" -e "ngrok http 5000"
-gnome-terminal --"/home/jacky/Desktop/WebRTC/WebRTC" -e "python3 Line.py"
+dbus-launch gnome-terminal --"/home/jacky/Desktop/WebRTC/WebRTC" -e "ngrok http 5000"
+dbus-launch gnome-terminal --"/home/jacky/Desktop/WebRTC/WebRTC" -e "python3 /home/jacky/Desktop/WebRTC/WebRTC/Line.py"
