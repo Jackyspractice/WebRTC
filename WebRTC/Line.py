faa4cb664a5c7d356ae2ed1cb46209e65e52fa5b
Jacky's Zenbook <s10828152@cycu.edu.tw>
2022-11-02 01:05:09 +0800

    scheme

--- WebRTC/Line.py
+++ WebRTC/Line.py
@@ -170,7 +170,7 @@ def open_port():
 
     print("open ngrok")
     ngrok.set_auth_token(ngrok_token)
-    http_tunnel_Line = ngrok.connect(addr = 5000, bind_tls = True)
+    http_tunnel_Line = ngrok.connect(addr = 5000, scheme = True)
     print("Line-------------------->" + http_tunnel_Line.public_url)
     http_tunnel_Server = ngrok.connect(addr = 8080, bind_tls = True)
     print("Server-------------------->" + http_tunnel_Server.public_url)
