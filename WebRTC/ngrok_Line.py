da2332d99bec636038200539749c12d4b9d66478
Jacky's Zenbook <s10828152@cycu.edu.tw>
2022-11-02 00:53:03 +0800

    http

--- WebRTC/ngrok_Line.py
+++ WebRTC/ngrok_Line.py
@@ -4,6 +4,6 @@ token = "2FM9hVmFjGxvAQLtqhzUouXBvMB_35eUbpExjAaERX4Rr7Bh2"
 
 
 ngrok.set_auth_token(token)
-http_tunnel = ngrok.connect(addr = 5000, bind_tls = True)
+http_tunnel = ngrok.connect(5000)
 print(http_tunnel.public_url)
 
