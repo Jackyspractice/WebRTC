6f872fefde5f35263ca2ea7211c04ea503b9ee6e
Jacky's Zenbook <s10828152@cycu.edu.tw>
2022-11-07 19:49:45 +0800

    original

--- WebRTC/server.py
+++ WebRTC/server.py
@@ -1,104 +1,120 @@
 import argparse
-import asyncio
-import json
 import logging
+import asyncio
 import os
+import uuid
+import json
 import platform
-import ssl
-import time
-import multiprocessing
 import signal
 
 from aiohttp import web
-
 from aiortc import RTCPeerConnection, RTCSessionDescription
 from aiortc.contrib.media import MediaPlayer, MediaRelay
-from aiortc.rtcrtpsender import RTCRtpSender
 
+import multiprocessing
+import time
+
+from pyngrok import ngrok
+
+#token = "2FM9hVmFjGxvAQLtqhzUouXBvMB_35eUbpExjAaERX4Rr7Bh2" #new
+token = "2EFlxQQDpreqVGMuQVTtTepMzHB_7Px3VmMDjcQxgLoDH7Ync" 
+logger = logging.getLogger("pc")
+pcs = set()
 ROOT = os.path.dirname(__file__)
-
-
-relay = None
+relays = None
 webcam = None
+vaild = "First"
+process_sleep = None
+main_pid = -1
 
+class parser:
 
-def create_local_tracks(play_from, decode):
-    global relay, webcam
+    args = None
 
-    if play_from:
-        player = MediaPlayer(play_from, decode=decode)
-        return player.audio, player.video
-    else:
-        options = {"framerate": "30", "video_size": "640x480"}
-        if relay is None:
-            if platform.system() == "Darwin":
-                webcam = MediaPlayer(
-                    "default:none", format="avfoundation", options=options
-                )
-            elif platform.system() == "Windows":
-                webcam = MediaPlayer(
-                    "video=Integrated Camera", format="dshow", options=options
-                )
-            else:
-                webcam = MediaPlayer("/dev/video1", format="v4l2", options=options)
-            relay = MediaRelay()
-        return None, relay.subscribe(webcam.video)
+    def set_logging_level(self):
 
+        if args.verbose:
+            logging.basicConfig(level = logging.DEBUG)
+            print("Debuge MODE")
+        else:
+            logging.basicConfig(level = logging.INFO)
+            print("INFO MODE")
 
-def force_codec(pc, sender, forced_codec):
-    kind = forced_codec.split("/")[0]
-    codecs = RTCRtpSender.getCapabilities(kind).codecs
-    transceiver = next(t for t in pc.getTransceivers() if t.sender == sender)
-    transceiver.setCodecPreferences(
-        [codec for codec in codecs if codec.mimeType == forced_codec]
-    )
+    def set_parser(self):
 
+        global args
+
+        parser = argparse.ArgumentParser(
+            description = "Webcam using WebRTC with python-lib aioRTC"
+        )
+
+        parser.add_argument(
+            "--host", "-IP", default = "127.0.0.1", help = "Host IP for HTTP Connection"
+        )
+
+        parser.add_argument(
+            "--port", type = int, default = 12501, help = "post number for server"
+        )
+
+        parser.add_argument("--verbose", "-v", action = "count")
+
+        args = parser.parse_args()
+
+        self.set_logging_level()
+
+def create_local_tracks():
+    global relays, webcam
+
+    options = {"framerate": "30", "video_size": "640x480"}
+    if relays is None:
+        if platform.system() == "Darwin":
+            webcam = MediaPlayer(
+                "default:none", format="avfoundation", options=options
+            )
+        elif platform.system() == "Windows":
+            webcam = MediaPlayer(
+                "video=USB2.0 HD UVC WebCam", format="dshow", options=options
+            )
+        else:
+            webcam = MediaPlayer("/dev/video1", format="v4l2", options=options)
+        relays = MediaRelay()
+    return relays.subscribe(webcam.video)
+
+async def on_shutdown(app):
+    # close peer connections
+    coros = [pc.close() for pc in pcs]
+    await asyncio.gather(*coros)
+    pcs.clear()
 
 async def index(request):
     content = open(os.path.join(ROOT, "index.html"), "r").read()
     return web.Response(content_type="text/html", text=content)
 
-
 async def javascript(request):
     content = open(os.path.join(ROOT, "client.js"), "r").read()
     return web.Response(content_type="application/javascript", text=content)
 
-
 async def offer(request):
     params = await request.json()
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
-
+    
     pc = RTCPeerConnection()
+    pc_id = "PeerConnection(%s)" % uuid.uuid4()
     pcs.add(pc)
 
-    @pc.on("connectionstatechange")
-    async def on_connectionstatechange():
-        print("Connection state is %s" % pc.connectionState)
-        if pc.connectionState == "failed":
-            await pc.close()
-            pcs.discard(pc)
+    def log_info(msg, *args):
+        logger.info(pc_id + " " + msg, *args)
 
-    # open media source
-    audio, video = create_local_tracks(
-        args.play_from, decode=not args.play_without_decoding
-    )
+    log_info("Created for %s", request.remote)
 
-    if audio:
-        audio_sender = pc.addTrack(audio)
-        if args.audio_codec:
-            force_codec(pc, audio_sender, args.audio_codec)
-        elif args.play_without_decoding:
-            raise Exception("You must specify the audio codec using --audio-codec")
+    video = create_local_tracks()
+    pc.addTrack(video)
 
-    if video:
-        video_sender = pc.addTrack(video)
-        if args.video_codec:
-            force_codec(pc, video_sender, args.video_codec)
-        elif args.play_without_decoding:
-            raise Exception("You must specify the video codec using --video-codec")
-
+     # handle offer
     await pc.setRemoteDescription(offer)
 
+
+    # send answer
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
 
@@ -109,15 +125,26 @@ async def offer(request):
         ),
     )
 
+def unvaild(request):
 
-pcs = set()
+    return "URL isn't vaild!"
 
+def set_page(app0):
 
-async def on_shutdown(app):
-    # close peer connections
-    coros = [pc.close() for pc in pcs]
-    await asyncio.gather(*coros)
-    pcs.clear()
+    app0.on_shutdown.append(on_shutdown)
+    app0.router.add_get("/", index)
+    app0.router.add_get("/client.js", javascript)
+    app0.router.add_post("/offer", offer)
+
+    return app0
+
+def get_ngrok_URL():
+
+    ngrok.set_auth_token(token)
+    http_tunnel = ngrok.connect(12501)
+    print("-------------------->" + http_tunnel.public_url)
+
+    return http_tunnel.public_url
 
 def sleep_kill(pid):
 
@@ -132,57 +159,35 @@ def sleep_kill(pid):
         print("Aleardy kill")
 
 
+def open_webcam():
+
+    
+    #setting INFO MODE
+    parser_setting = parser()
+    parser_setting.set_parser()
+
+    #setting website response
+    app = web.Application()
+    app = set_page(app)
+
+    #input argument
+    web.run_app(
+        app, access_log=None, host=args.host, port=args.port, ssl_context=None
+    )
+    
+
+    
 if __name__ == "__main__":
+
 
     main_pid = os.getpid()
     process_sleep = multiprocessing.Process(target = sleep_kill, args=(main_pid,))
     process_sleep.start()
 
     print("start main")
-    parser = argparse.ArgumentParser(description="WebRTC webcam demo")
-    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
-    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
-    parser.add_argument("--play-from", help="Read the media from a file and sent it."),
-    parser.add_argument(
-        "--play-without-decoding",
-        help=(
-            "Read the media without decoding it (experimental). "
-            "For now it only works with an MPEGTS container with only H.264 video."
-        ),
-        action="store_true",
-    )
-    parser.add_argument(
-        "--host", default="127.0.0.1", help="Host for HTTP server (default: 0.0.0.0)"
-    )
-    parser.add_argument(
-        "--port", type=int, default=12501, help="Port for HTTP server (default: 8080)"
-    )
-    parser.add_argument("--verbose", "-v", action="count")
-    parser.add_argument(
-        "--audio-codec", help="Force a specific audio codec (e.g. audio/opus)"
-    )
-    parser.add_argument(
-        "--video-codec", help="Force a specific video codec (e.g. video/H264)"
-    )
-
-    args = parser.parse_args()
-
-    if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.INFO)
-
-    if args.cert_file:
-        ssl_context = ssl.SSLContext()
-        ssl_context.load_cert_chain(args.cert_file, args.key_file)
-    else:
-        ssl_context = None
-
-    app = web.Application()
-    app.on_shutdown.append(on_shutdown)
-    app.router.add_get("/", index)
-    app.router.add_get("/client.js", javascript)
-    app.router.add_post("/offer", offer)
-    web.run_app(app, host=args.host, port=args.port, ssl_context=ssl_context)
+    open_webcam()
 
 
+
+
+    
